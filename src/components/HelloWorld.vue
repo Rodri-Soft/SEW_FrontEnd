84f72af5defcb7f14507c602f4ddb320ba2d9616
Rodrigo <sanchezcpvx@gmail.com>
2022-11-02 18:53:08 -0600

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,59 +1,167 @@
 <template>
+
+<MDBNavbar expand="lg" light bg="light" container>
+    <MDBNavbarBrand href="#">Brand</MDBNavbarBrand>
+    <MDBNavbarToggler
+      @click="collapse1 = !collapse1"
+      target="#navbarSupportedContent"
+    ></MDBNavbarToggler>
+    <MDBCollapse v-model="collapse1" id="navbarSupportedContent">
+      <MDBNavbarNav class="mb-2 mb-lg-0">
+        <MDBNavbarItem to="#" active>
+          Home
+        </MDBNavbarItem>
+        <MDBNavbarItem href="#">
+          Link
+        </MDBNavbarItem>
+        <MDBNavbarItem>
+          <!-- Navbar dropdown -->
+          <MDBDropdown class="nav-item" v-model="dropdown1">
+            <MDBDropdownToggle
+              tag="a"
+              class="nav-link"
+              @click="dropdown1 = !dropdown1"
+              >Dropdown</MDBDropdownToggle
+            >
+            <MDBDropdownMenu aria-labelledby="dropdownMenuButton">
+              <MDBDropdownItem href="#">Action</MDBDropdownItem>
+              <MDBDropdownItem href="#">Another Action</MDBDropdownItem>
+              <MDBDropdownItem href="#"
+                >Something else here</MDBDropdownItem
+              >
+            </MDBDropdownMenu>
+          </MDBDropdown>
+        </MDBNavbarItem>
+        <MDBNavbarItem to="#" disabled>
+          Disabled
+        </MDBNavbarItem>
+        
+        <MDBNavbarItem to="#" disabled>
+          <a href="">
+            <MDBIcon icon="envelope" size="lg"></MDBIcon>
+            <MDBBadge pill notification class="bg-danger">1</MDBBadge>
+          </a>
+        </MDBNavbarItem>
+
+        <MDBNavbarItem to="#">
+          <MDBDropdown v-model="dropdown2">
+            <!-- <MDBDropdownToggle > -->
+            <a href="#" @click="dropdown2 = !dropdown2">
+              <MDBIcon icon="envelope" size="lg"></MDBIcon>
+              <MDBBadge pill notification class="bg-danger">1</MDBBadge>
+            </a>
+            <!-- </MDBDropdownToggle> -->
+            <MDBDropdownMenu aria-labelledby="dropdownMenuButton">
+              <MDBDropdownItem href="#">Action</MDBDropdownItem>
+              <MDBDropdownItem href="#">Another Action</MDBDropdownItem>
+              <MDBDropdownItem href="#">Something else here</MDBDropdownItem>
+            </MDBDropdownMenu>
+          </MDBDropdown>
+        </MDBNavbarItem>
+                
+      </MDBNavbarNav>
+      <!-- Search form -->
+
+      
+      <form class="d-flex input-group w-auto">
+        
+        <input
+          type="search"
+          class="form-control"
+          placeholder="Type query"
+          aria-label="Search"
+        />
+        <MDBBtn outline="primary">
+          Search
+        </MDBBtn>
+      </form>
+    </MDBCollapse>
+  </MDBNavbar>
+
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <MDBContainer>
+      <div
+        class="d-flex justify-content-center align-items-center"
+        style="height: 100vh"
+      >
+        <div class="text-center">
+          <img
+            class="mb-4"
+            src="https://mdbootstrap.com/img/logo/mdb-transparent-250px.png"
+            style="width: 250px; height: 90px"
+          />
+          <h5 class="mb-3">{{ msg }}</h5>
+          <p class="mb-3">MDB Team</p>
+          <p class="mt-5 mb-3">
+            PS. We'll be releasing "How to build your first project with MDB 5
+            Vue" tutorial soon. Make sure to join our newsletter not to miss it!
+          </p>
+          <a
+            class="btn btn-primary btn-lg"
+            href="https://mdbootstrap.com/newsletter/"
+            target="_blank"
+            role="button"
+            >Join now</a
+          >
+        </div>
+      </div>
+    </MDBContainer>
   </div>
 </template>
 
 <script>
+// import { MDBContainer } from "mdb-vue-ui-kit";
+import { ref } from 'vue';
+import {
+  MDBContainer,
+  MDBBtn,
+  MDBNavbar,
+  MDBNavbarToggler,
+  MDBNavbarBrand,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBCollapse,
+  MDBDropdown,
+  MDBDropdownToggle,
+  MDBDropdownMenu,
+  MDBDropdownItem,
+  MDBBadge,
+  MDBIcon
+} from 'mdb-vue-ui-kit';
+
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
+  components: {
+    MDBContainer,
+    MDBBtn,
+    MDBNavbar,
+    MDBNavbarToggler,
+    MDBNavbarBrand,
+    MDBNavbarNav,
+    MDBNavbarItem,
+    MDBCollapse,
+    MDBDropdown,
+    MDBDropdownToggle,
+    MDBDropdownMenu,
+    MDBDropdownItem,
+    MDBBadge,
+    MDBIcon
+  },
   props: {
     msg: String
+  },
+  setup() {
+    const collapse1 = ref(false);
+    const dropdown1 = ref(false);
+    const dropdown2 = ref(false);
+    return {
+      collapse1,
+      dropdown1,
+      dropdown2
+    }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<style scoped></style>
