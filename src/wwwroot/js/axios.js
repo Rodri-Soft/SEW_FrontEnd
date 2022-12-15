2c29d74ed4a1e997c645ec21794efa106c8c7043
camarillovilla <dukecamarillo@gmail.com>
2022-12-15 05:41:33 -0600

    Base url change

--- src/wwwroot/js/axios.js
+++ src/wwwroot/js/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import Cookies from 'js-cookie';
 
-axios.defaults.baseURL = 'http://localhost:3000/api/v1/';
+axios.defaults.baseURL = 'https://sewapi-production.up.railway.app/api/v1/';
 
 // axios.defaults.headers.common['Authorization'] = 'Bearer ' + Cookies.get("access_token");
